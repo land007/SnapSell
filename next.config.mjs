b2bee334ad9f512775252d3809aeee2f5a31aa74
阿七 <yiqiujia@hotmail.com>
2025-11-24 15:21:52 +0800

    Enable static export for Webify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // 启用静态导出
+  images: {
+    unoptimized: true, // 静态导出需要禁用图片优化
+  },
 };
 
 export default nextConfig;
